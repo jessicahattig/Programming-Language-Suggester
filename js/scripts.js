b406e7e74c7565c273483d132dfbfc5cd8fbd927
jessicahattig <jessicahattig@gmail.com>
2023-06-16 16:54:46 -0700

    WIP: compute survey result.

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,32 @@ window.addEventListener("load", function() {
   let resetBtn = document.querySelector("button#reset");
   let results = document.querySelector("div#surveyResults");
 
-
 form.addEventListener("submit", function(event) {
-  const question1 = document.querySelector("question1Input").value;
-  const question2 = document.querySelector("question2Input").value;
-  const question3 = document.querySelector("question3Input").value;
-  const question4 = document.querySelector("question4Input").value;
-  const question5 = document.querySelector("question5Input").value;
+  event.preventDefault();
+  const question1 = document.getElementById("question1Input").value;
+  const question2 = document.getElementById("question2Input").value;
+  const question3 = document.getElementById("question3Input").value;
+  const question4 = document.getElementById("question4Input").value;
+  const question5 = document.getElementById("question5Input").value;
+
+let result;
+if (question1 === "Yes") {
+  result = "Python!";
+} else if (question2 === "Yes") {
+  result = "Ruby!";
+} else if (question3 === "Yes"){
+  result = "Javascript!"
+} else if (question4 === "Yes"){
+  result = "Go!"
+} else if (question5 === "Yes"){
+  result = "C#!"
 }
+
+results.removeAttribute("class");
+event.preventDefault();
+
+document.getElementById("output").inerText = result;
+)};
 
 
 
