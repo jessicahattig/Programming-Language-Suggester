054e59a57bd8c6008c4c0c1283a2af847671242f
jessicahattig <jessicahattig@gmail.com>
2023-06-16 16:32:05 -0700

    Listen to reset.

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,23 @@
 
 
 
+window.addEventListener("load", function() {
+  let form = document.getElementById("form");
+  let resetBtn = document.querySelector("button#reset");
+  let results = document.querySelector("div#surveyResults");
+
+
+  // form.addEventListener("submit", calcSurvey);
+
 
 
 function calcSurvey(event) {
   event.preventDefault();
-  const
+  const question1 = document.querySelector("question1Input").value;
+  const question2 = document.querySelector("question2Input").value;
+  const question3 = document.querySelector("question3Input").value;
+  const question4 = document.querySelector("question4Input").value;
+  const question5 = document.querySelector("question5Input").value;
 }
 
-
-
-
-
-
-window.addEventListener("load", function() {
-  const form = document.getElementById("questionnaire")
-  form.addEventListener("submit", calcSurvey);
 });
