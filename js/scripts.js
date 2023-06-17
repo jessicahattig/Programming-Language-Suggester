1bd37970f9ba8ea251bffab7d2ffaed2848fc61e
jessicahattig <jessicahattig@gmail.com>
2023-06-17 16:29:22 -0700

    Call the function to determine the recommended language.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,19 @@ window.addEventListener("load", function() {
   const reveal = document.getElementById("results");
   const results = document.getElementById("output");
 
-
+//Add event listener to the form submit event
 form.addEventListener("submit", function(event) {
   event.preventDefault();
+
+//Getting Survey responses
   const question1 = document.getElementById("question1Input").value;
   const question2 = document.getElementById("question2Input").value;
   const question3 = document.getElementById("question3Input").value;
   const question4 = document.getElementById("question4Input").value;
   const question5 = document.getElementById("question5Input").value;
+
+// Calling the function to determine the recommended language.
+  const recommendedLanguage = determineRecommendedLanguage(question1, question2, question3, question4, question5);
 
 let result;
 if (question1 === "Yes") {
