b1c024f28b0faeace0157107a196c1a1ecb2b262
jessicahattig <jessicahattig@gmail.com>
2023-06-17 16:44:36 -0700

    Show console log message when form is submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,14 @@ window.addEventListener("load", function() {
 //Add event listener to the form submit event
 form.addEventListener("submit", function(event) {
   event.preventDefault();
+  console.log("HOLA");
 
 //Getting Survey responses
-  const question1 = document.getElementById("question1Input").value;
-  const question2 = document.getElementById("question2Input").value;
-  const question3 = document.getElementById("question3Input").value;
-  const question4 = document.getElementById("question4Input").value;
-  const question5 = document.getElementById("question5Input").value;
+  const q1 = document.querySelector("question1Input").value;
+  const q2 = document.querySelector("question2Input").value;
+  const q3 = document.querySelector("question3Input").value;
+  const q4 = document.querySelector("question4Input").value;
+  const q5 = document.querySelector("question5Input").value;
 
 //Call the function to determine the recommended language.
   const recommendedLanguage = determineRecommendedLanguage(question1, question2, question3, question4, question5);
@@ -26,18 +27,28 @@ document.getElementById("output").inerText = recommendedLanguage;
 results.removeAttribute("class");
 });
 
-let result;
-if (question1 === "Yes") {
-  result = "Python!";
-} else if (question2 === "Yes") {
-  result = "Ruby!";
-} else if (question3 === "Yes"){
-  result = "Javascript!"
-} else if (question4 === "Yes"){
-  result = "Go!"
-} else if (question5 === "Yes"){
-  result = "C#!"
+//Function to determine the recommended language based on survey results
+function determineRecommendedLanguage (q1, q1, q3, q4, q5) {
+  if (q1 ==="Yes" && q2 ==="Yes" && q3 ==="Yes") {
+    return "Python"
+  } else if (q4 ==="Yes" && q5 ==="Yes") {
+    return "Javascript"
+  } else {
+    return "C#"
+  }
 }
+
+// if (question1 === "Yes") {
+//   result = "Python!";
+// } else if (question2 === "Yes") {
+//   result = "Ruby!";
+// } else if (question3 === "Yes"){
+//   result = "Javascript!"
+// } else if (question4 === "Yes"){
+//   result = "Go!"
+// } else if (question5 === "Yes"){
+//   result = "C#!"
+// }
 
 
 
