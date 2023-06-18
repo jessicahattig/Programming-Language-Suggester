6d28dbff949f4fe2a7ff7f85c5ea55e08e76bb56
jessicahattig <jessicahattig@gmail.com>
2023-06-17 17:01:10 -0700

    WIP: Branch results.

--- js/scripts.js
+++ js/scripts.js
@@ -8,35 +8,36 @@ window.addEventListener("load", function() {
 //Add event listener to the form submit event
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log("HOLA");
 
-//Getting Survey responses
-  const q1 = document.querySelector("question1Input").value;
-  const q2 = document.querySelector("question2Input").value;
-  const q3 = document.querySelector("question3Input").value;
-  const q4 = document.querySelector("question4Input").value;
-  const q5 = document.querySelector("question5Input").value;
+//Get Survey responses
+  const q1 = document.getElementById("question1Input").value;
+  const q2 = document.getElementById("question2Input").value;
+  const q3 = document.getElementById("question3Input").value;
+  const q4 = document.getElementById("question4Input").value;
+  const q5 = document.getElementById("question5Input").value;
 
 //Call the function to determine the recommended language.
-  const recommendedLanguage = determineRecommendedLanguage(question1, question2, question3, question4, question5);
-
-//Display recommended language
-document.getElementById("output").inerText = recommendedLanguage;
-
-//Show the results section
-results.removeAttribute("class");
-});
+  const recommendedLanguage = determineRecommendedLanguage(q1, q2, q3, q4, q5);
 
 //Function to determine the recommended language based on survey results
 function determineRecommendedLanguage (q1, q1, q3, q4, q5) {
-  if (q1 ==="Yes" && q2 ==="Yes" && q3 ==="Yes") {
-    return "Python"
-  } else if (q4 ==="Yes" && q5 ==="Yes") {
-    return "Javascript"
+  if (q1 ==="Yes") {
+    result = Python;
+  } else if (q2 ==="Yes") {
+    result = Ruby;
   } else {
     return "C#"
   }
 }
+
+//Display recommended language
+document.getElementById("output").inerText = recommendedLanguage;
+
+//Show the result section
+results.removeAttribute("class");
+});
+
+
 
 // if (question1 === "Yes") {
 //   result = "Python!";
@@ -57,9 +58,9 @@ function determineRecommendedLanguage (q1, q1, q3, q4, q5) {
 
 
 
-function calcSurvey(event) {
-  event.preventDefault();
+// function calcSurvey(event) {
+//   event.preventDefault();
   
-}
+// }
 
-});
+// });
