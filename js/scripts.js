90f6d7acef41e1339b1a216a37ce7c59c6760756
jessicahattig <jessicahattig@gmail.com>
2023-06-17 19:16:33 -0700

    BUGFIX: Output shown.

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,18 @@ const c = "C# (pronounced C-sharp) is a statically-typed language developed by M
 
 //Function to determine the recommended language based on survey responses
 function calc(event) {
-  let recommendedLanguage = document.getElementById("bigreveal");
-  recommendedLanguage.removeAttribute("class")
   event.preventDefault();
 
 
   //Get Survey values
-  const q1 = document.getElementById("question1Input").value;
-  const q2 = document.getElementById("question2Input").value;
-  const q3 = document.getElementById("question3Input").value;
-  const q4 = document.getElementById("question4Input").value;
-  const q5 = document.getElementById("question5Input").value;
+  const q1 = document.getElementById("question1").value;
+  const q2 = document.getElementById("question2").value;
+  const q3 = document.getElementById("question3").value;
+  const q4 = document.getElementById("question4").value;
+  const q5 = document.getElementById("question5").value;
 
   //Branching
-  let calc;
+  let result;
   if (q1 ==="Yes") {
     result = python;
   } else if (q2 === "Yes") {
@@ -44,10 +42,13 @@ function calc(event) {
     result = js;
   }
 
-document.getElementById("result").innerText =calc;
+document.getElementById("result").innerText = result;
+
+let recommendedLanguage = document.getElementById("bigreveal");
+  recommendedLanguage.removeAttribute("class")
 }
 
-window.addEventListener("load", function(){ //Wait for the DOM to load
+window.addEventListener("load", function() { //Wait for the DOM to load
   const form = document.querySelector("form"); //Get References to the form
   form.addEventListener("submit", calc)
 });
