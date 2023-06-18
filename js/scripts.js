a8d466471de42a2213af933831ac66a4b93d6d5e
jessicahattig <jessicahattig@gmail.com>
2023-06-17 21:05:10 -0700

    Remove commented out notes in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
-//Results options
+
 const python = "Python is a versatile, high-level language known for its simplicity and readability. It has a large standard library and a vibrant community. Python is widely used for web development, scientific computing, data analysis, machine learning, and automation."
 const js = "JavaScript is a versatile, interpreted language primarily used for front-end web development. It allows for interactive and dynamic web content, including animations, user interfaces, and browser-based applications. JavaScript is also increasingly popular for back-end development using frameworks like Node.js."
 const c = "C# (pronounced C-sharp) is a statically-typed language developed by Microsoft. It is commonly used for building Windows applications, web applications, and games using the .NET framework. C# emphasizes strong typing, scalability, and performance."
 
-window.addEventListener("load", function () { //Wait for the DOM to load
-  const form = document.querySelector("form"); //Get References to the form
+window.addEventListener("load", function () { 
+  const form = document.querySelector("form"); 
   form.addEventListener("submit", calc)
   let resetBtn = document.querySelector("button#reset");
 
 
-  //Function to determine the recommended language based on survey responses
   function calc(event) {
     event.preventDefault();
 
 
-    //Get Survey values
+   
     const q1 = document.getElementById("question1").value;
     const q2 = document.getElementById("question2").value;
     const q3 = document.getElementById("question3").value;
     const q4 = document.getElementById("question4").value;
     const q5 = document.getElementById("question5").value;
 
-    //Branching
+
     let result;
     if (q1 === "Yes") {
       result = python;
