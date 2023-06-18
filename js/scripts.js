cfa876710b2c0630d044f1611e76f02355ab61d3
jessicahattig <jessicahattig@gmail.com>
2023-06-17 17:13:59 -0700

    Create resul options as variables.

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,13 @@ document.getElementById("output").inerText = recommendedLanguage;
 results.removeAttribute("class");
 });
 
+//Results options
+const python = "Python is a versatile, high-level language known for its simplicity and readability. It has a large standard library and a vibrant community. Python is widely used for web development, scientific computing, data analysis, machine learning, and automation."
+const ruby = "Ruby is a dynamic, interpreted language known for its simplicity and productivity. It has an elegant syntax that focuses on readability and is often used for web development, scripting, and prototyping."
+const js = "JavaScript is a versatile, interpreted language primarily used for front-end web development. It allows for interactive and dynamic web content, including animations, user interfaces, and browser-based applications. JavaScript is also increasingly popular for back-end development using frameworks like Node.js."
+const go = "Go (often referred to as Golang) is a statically-typed, compiled language developed by Google. It is designed for efficiency and simplicity, offering built-in support for concurrency. Go is often used for developing scalable network applications, system tools, and distributed systems."
+const c = "C# (pronounced C-sharp) is a statically-typed language developed by Microsoft. It is commonly used for building Windows applications, web applications, and games using the .NET framework. C# emphasizes strong typing, scalability, and performance."
+
 
 
 // if (question1 === "Yes") {
